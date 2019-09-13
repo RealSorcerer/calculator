17b63c8510c3ad439f782535449013f6d268e448
Olga Ruban <joveolgaa@gmail.com>
2019-09-13 17:24:05 +0300

    fixed problems

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,36 +1,24 @@
-import java.util.HashMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Calculator {
-    public final static HashMap<String, Integer> ROMAN_NUMERALS;
-    static {
-        ROMAN_NUMERALS = new HashMap<>();
-        ROMAN_NUMERALS.put("I", 1);
-        ROMAN_NUMERALS.put("II", 2);
-        ROMAN_NUMERALS.put("III", 3);
-        ROMAN_NUMERALS.put("IV", 4);
-        ROMAN_NUMERALS.put("V", 5);
-        ROMAN_NUMERALS.put("VI", 6);
-        ROMAN_NUMERALS.put("VII", 7);
-        ROMAN_NUMERALS.put("VIII", 8);
-        ROMAN_NUMERALS.put("IX", 9);
-        ROMAN_NUMERALS.put("X", 10);
-    }
 
-    public static Number calculate(String str) throws IllegalArgumentException {
+    public static String calculate(String str) throws IllegalArgumentException {
         Pattern pattern = Pattern.compile("^\\s*?(10|\\d)\\s*?([+-/*])\\s*?(10|\\d)\\s*?$|" +
                 "^\\s*?(I{1,3}|V|X|VI{1,3}|IV|IX)\\s*?([+-/*])\\s*?(I{1,3}|V|X|VI{1,3}|IV|IX)\\s*?$");
         Matcher matcher = pattern.matcher(str);
+        double result = 0;
+        boolean isRoman = false;
 
         if (matcher.find()) {
-            Integer a;
+            int a;
             String operation;
-            Integer b;
+            int b;
             if (matcher.group(1) == null) {
-                a = ROMAN_NUMERALS.get(matcher.group(4));
+                a = getNumber(matcher.group(4));
                 operation = matcher.group(5);
-                b = ROMAN_NUMERALS.get(matcher.group(6));
+                b = getNumber(matcher.group(6));
+                isRoman = true;
             } else {
                 a = Integer.parseInt(matcher.group(1));
                 operation = matcher.group(2);
@@ -39,20 +27,89 @@ public class Calculator {
 
             switch (operation) {
                 case "+" :
-                    return a + b;
+                    result = a + b;
+                    break;
                 case "-" :
-                    return a - b;
+                    result = a - b;
+                    break;
                 case "*" :
-                    return a * b;
+                    result = a * b;
+                    break;
                 case "/" :
                     if (b == 0) {
                         throw new IllegalArgumentException("Деление на ноль.");
                     }
-                    return (double) a / b;
-                default: return 0;
+                    result = (double) a / b;
+                    break;
             }
         } else {
             throw new IllegalArgumentException("Не верный формат.");
         }
+        if (isRoman) {
+            return getRomanResult((int) result);
+        }
+        return String.valueOf(result);
+    }
+
+    public static String getRomanResult(int number) {
+        StringBuilder builder = new StringBuilder();
+
+        if (number == 100) {
+            builder.append("C");
+            number = number - 100;
+        }
+        if (number >= 90) {
+            builder.append("XC");
+            number -= 90;
+        }
+        if (number >= 50) {
+            builder.append("L");
+            number -= 50;
+        }
+        if (number >= 40) {
+            builder.append("XL");
+            number -= 40;
+        }
+        while (number >= 10) {
+            builder.append("X");
+            number -= 10;
+        }
+        if (number == 9) {
+            builder.append("IX");
+            return builder.toString();
+        }
+        if (number >= 5) {
+            builder.append("V");
+            number -= 5;
+        }
+        if (number == 4) {
+            builder.append("IV");
+            return builder.toString();
+        }
+        while (number > 0) {
+            builder.append("I");
+            number -= 1;
+        }
+
+        return builder.toString();
+    }
+
+    public static int getNumber(String stringNumber) {
+        int result = 0;
+        int k = 1;
+        for (int i = stringNumber.length() - 1; i >= 0; i--) {
+            if (stringNumber.charAt(i) == 'I') {
+                result += 1 * k;
+            }
+            if (stringNumber.charAt(i) == 'X') {
+                result += 10;
+                k = -1;
+            }
+            if (stringNumber.charAt(i) == 'V') {
+                result += 5;
+                k = -1;
+            }
+        }
+        return result;
     }
 }
